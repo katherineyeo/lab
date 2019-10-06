b6bded21fca52a5dd532f3e5fac28c25afcc4b57
kaythi73 <kaythi@titansoft.com.sg>
2019-10-06 12:12:58 +0800

    1. Separate the compare and swap element logic 2. Move to combo comparer 3. Rearrange logic in compare 4. Rename param 1 and param 2 as x and y 5. Add interface IComparer 6. Make first and second comparer to private

---
+++ CSharpAdvanceDesignTests/ComboComparer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Lab.Entities;
+
+namespace CSharpAdvanceDesignTests
+{
+    public class ComboComparer : IComparer<Employee>
+    {
+        public ComboComparer(IComparer<Employee> firstComparer, IComparer<Employee> secondComparer)
+        {
+            FirstComparer = firstComparer;
+            SecondComparer = secondComparer;
+        }
+
+        private IComparer<Employee> FirstComparer { get; set; }
+        private IComparer<Employee> SecondComparer { get; set; }
+
+        public int Compare(Employee x, Employee y)
+        {
+            var firstCompareResult = FirstComparer.Compare(x, y);
+            if (firstCompareResult != 0)
+            {
+                return firstCompareResult;
+            }
+            return SecondComparer.Compare(x, y);
+        }
+    }
+}
--- CSharpAdvanceDesignTests/JoeyOrderByTests.cs
+++ CSharpAdvanceDesignTests/JoeyOrderByTests.cs
@@ -1,31 +1,17 @@
 ﻿using ExpectedObjects;
 using Lab.Entities;
 using NUnit.Framework;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace CSharpAdvanceDesignTests
 {
-    public class ComboComparer
-    {
-        public ComboComparer(IComparer<Employee> combineKeyComparer, IComparer<Employee> secondCombinedKeyComparer)
-        {
-            CombineKeyComparer = combineKeyComparer;
-            SecondCombinedKeyComparer = secondCombinedKeyComparer;
-        }
-
-        public IComparer<Employee> CombineKeyComparer { get; private set; }
-        public IComparer<Employee> SecondCombinedKeyComparer { get; private set; }
-    }
-
     [TestFixture]
     public class JoeyOrderByTests
     {
         [Test]
         public void orderBy_lastName_and_firstName()
         {
-
             var employees = new[]
             {
                 new Employee {FirstName = "Joey", LastName = "Wang"},
@@ -33,11 +19,12 @@ namespace CSharpAdvanceDesignTests
                 new Employee {FirstName = "Joseph", LastName = "Chen"},
                 new Employee {FirstName = "Joey", LastName = "Chen"},
             };
-
-
-            Func<Employee, string> secondKeySelector = employee => employee.FirstName;
-            IComparer<string> secondKeyComparer = Comparer<string>.Default;
-            var actual = JoeyOrderByLastNameAndFirstName(employees, new ComboComparer(new CombineKeyComparer(employee => employee.LastName, Comparer<string>.Default), new CombineKeyComparer(secondKeySelector, secondKeyComparer)));
+            
+            var actual = JoeyOrderByLastNameAndFirstName(employees,
+                new ComboComparer(
+                    new CombineKeyComparer(employee => employee.LastName, Comparer<string>.Default),
+                    new CombineKeyComparer(employee => employee.FirstName, Comparer<string>.Default))
+                );
 
             var expected = new[]
             {
@@ -51,7 +38,7 @@ namespace CSharpAdvanceDesignTests
         }
 
         private IEnumerable<Employee> JoeyOrderByLastNameAndFirstName(
-            IEnumerable<Employee> employees, 
+            IEnumerable<Employee> employees,
             ComboComparer comboComparer)
         {
             //bubble sort
@@ -63,19 +50,14 @@ namespace CSharpAdvanceDesignTests
                 for (int i = 1; i < elements.Count; i++)
                 {
                     var employee = elements[i];
-                    if (comboComparer.CombineKeyComparer.Compare(employee, minElement) < 0)
+                    var finalCompareResult = comboComparer.Compare(employee, minElement);
+
+                    if (finalCompareResult < 0)
                     {
                         minElement = employee;
                         index = i;
                     }
-                    else if (comboComparer.CombineKeyComparer.Compare(employee, minElement) == 0)
-                    {
-                        if (comboComparer.SecondCombinedKeyComparer.Compare(employee, minElement) < 0)
-                        {
-                            minElement = employee;
-                            index = i;
-                        }
-                    }
+
                 }
 
                 elements.RemoveAt(index);
